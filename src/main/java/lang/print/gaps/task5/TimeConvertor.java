8a3d02fa525e9e44538b315ea98e8c69cbda1a77
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-08-29 18:13:19 +0600

    Task #5. Operators

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    float seconds = 0;
     public void convert(float minutes) {
+        seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
